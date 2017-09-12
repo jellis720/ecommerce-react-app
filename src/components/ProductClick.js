2f48176c9325e6a8921d243fd2c2d792173114b0
jellis720 <jellis720@gmail.com>
2017-09-12 19:57:46 -0400

    attempt2GH

--- src/components/ProductClick.js
+++ src/components/ProductClick.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import BaseLayout from "./BaseLayout.js"
+import Products from "./Products.js"
 
 export default class ProductClick extends Component {
   render(){
     return(
       <div>
       <BaseLayout>
-        <h1 className="innerContent">Test Product Page</h1>
+        <h1 className="innerContent">Product Details Page</h1>
       </BaseLayout>
       </div>
     )
